cf5d3f2ba6856512e99d36d8af10a66621fd7f55
Kyle Padayachee <kyle44987@gmail.com>
2025-05-10 14:07:58 +0200

    added course models

--- myVenv/myProject/myApp/models.py
+++ myVenv/myProject/myApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
 
+    def __str__(self):
+        return self.title
+    
+    class UserProgress(models.Model):
+        user = models.ForeignKey(User, on_delete=models.CASCADE)
+        course = models.ForeignKey(Course, on_delete=models.CASCADE)
+        completed = models.BooleanField(default=False)   
+
+        def __str__(self):
+            return f"{self.user.username} - {self.course.title}"
